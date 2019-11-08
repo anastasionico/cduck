1093c41d3047345258a3c3e111836737abc3dd48
anastasionico <nico@anastasionico.uk>
2019-11-08 14:23:50 +0000

    migration+seeding+adminLTE installed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cyber Duck
+                    Cyber Ducks 
                 </div>
 
                 <div class="links">
